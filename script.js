1be43c7ec25e8f505836fa5a90018ad3a3660574
Nufililicek <nufililicek@gmail.com>
2026-03-11 19:41:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-// proměnné
 let otazky = [];
 let aktualni = 0;
+let body = 0;
+
+// funkce na odstranění diakritiky
+function odstranDiakritiku(text) {
+  return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
 
 // načtení textového souboru
 fetch("figury_tropy.txt")
   .then(r => r.text())
   .then(data => {
-    // rozdělíme podle řádků
     let radky = data.split("\n");
-
-    // každý řádek rozdělíme podle "|"
     otazky = radky.map(r => {
       let casti = r.split("|");
       return {question: casti[0], answer: casti[1]};
     });
-
     zobraz();
   });
 
@@ -23,10 +24,14 @@ function zobraz() {
   if (aktualni < otazky.length) {
     document.getElementById("otazka").innerText =
       otazky[aktualni].question;
+    document.getElementById("odpoved").value = "";
+    document.getElementById("vysledek").innerText = "";
   } else {
-    document.getElementById("otazka").innerText =
-      "Kvíz skončil!";
+    document.getElementById("otazka").innerText = "Kvíz skončil!";
     document.getElementById("odpoved").style.display = "none";
+    document.getElementById("vysledek").style.display = "none";
+    document.getElementById("skore").innerText =
+      "Správných odpovědí: " + body + " z " + otazky.length;
   }
 }
 
@@ -34,13 +39,16 @@ function zobraz() {
 function zkontroluj() {
   let odpoved = document.getElementById("odpoved").value;
 
-  if (odpoved == otazky[aktualni].answer) {
+  let odpovedCista = odstranDiakritiku(odpoved.toLowerCase());
+  let spravnaCista = odstranDiakritiku(otazky[aktualni].answer.toLowerCase());
+
+  if (odpovedCista == spravnaCista) {
     document.getElementById("vysledek").innerText = "Správně";
+    body++;
   } else {
-    document.getElementById("vysledek").innerText = "Špatně";
+    document.getElementById("vysledek").innerText = "Špatně, správně: " + otazky[aktualni].answer;
   }
 
   aktualni++;
-  document.getElementById("odpoved").value = "";
   zobraz();
 }
